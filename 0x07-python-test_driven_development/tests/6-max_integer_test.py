a505256f9bb730b3e0b46324779f89e929be0fd0
FrancisOkolo <francischinedu78@gmail.com>
2022-11-24 07:56:46 -0800

    task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,11 +8,14 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_arg(self):
         """to test for various arguments"""
-        self.assertAlmostEqual(max_integer([], None))
-        self.assertAlmostEqual(max_integer([2, 3, 4, 5, 6], 6))
-        self.assertAlmostEqual(max_integer([2, 4, 6, 6, 8], 8))
-        self.assertAlmostEqual(max_integer([5], 5))
-        self.assertAlmostEqual(max_integer([2.5, 3.8, 7, 8.3], 8.3))
+
+        self.assertAlmostEqual(max_integer([2, 3, 4, 5, 6]), 6)
+        self.assertAlmostEqual(max_integer([2, 4, 9, 6, 8]), 9)
+        self.assertAlmostEqual(max_integer([12, 2, 5, 8, 9]), 12)
+        self.assertAlmostEqual(max_integer([10, 8, -7, 5, 3]), 10)
+        self.assertAlmostEqual(max_integer([-2, -4, -6, -8, -10]), -2)
+        self.assertAlmostEqual(max_integer([5]), 5)
+        self.assertAlmostEqual(max_integer([2.5, 3.8, 7, 8.3]), 8.3)
 
     def test_argType(self):
         self.assertRaises(TypeError, max_integer, [2, 3, 6, 'sring'])
@@ -20,3 +23,11 @@ class TestMaxInteger(unittest.TestCase):
         self.assertRaises(TypeError, max_integer, [2, 3, 6, 2 + 5j])
         self.assertRaises(TypeError, max_integer, [2, 3, 6,], [7, 8, 9])
         self.assertRaises(TypeError, max_integer, [2, 3, 6, {'no': 8, 'yes': 12}])
+
+    def test_None(self):
+        self.assertIsNone(max_integer([]), None)
+        self.assertIsNone(max_integer(), None)
+        self.assertIsNone(max_integer(None), None)
+
+if __name__ == '__main__':
+    unittest.main()
