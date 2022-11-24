dedc5df1ca133bfd640a32fb8e22689aaa680ca7
FrancisOkolo <francischinedu78@gmail.com>
2022-11-24 03:27:30 -0800

    task 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,17 +5,16 @@
 """
 
 
-
 def text_indentation(text):
     """function to create indentations"""
     if type(text) is not str:
-        raise TypeError('text must be string')
+        raise TypeError('text must be a string')
     else:
         for char in ".?:":
             # replaces .?: with itself and newline
             text = text.replace(char, char + "\n\n")
         # changes string to list to strip whitspaces
-        list_lines =[lines.strip(' ') for lines in text.split('\n')]
+        list_lines = [lines.strip(' ') for lines in text.split('\n')]
         # returns to string and returns newline
         revised = '\n'.join(list_lines)
         print(revised, end='')
