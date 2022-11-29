1309c352614ece0ac8c9a25757e196c711db24a7
FrancisOkolo <francischinedu78@gmail.com>
2022-11-29 05:14:07 +0000

    task 6

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,9 +4,9 @@ const process = require('process');
 
 const myVar = process.argv;
 if (myVar[2] === undefined) {
-  console.log(`${myVar} is undefined`);
+  console.log(`${myVar[2]} is undefined`);
 } else if (myVar.length === 3){
     console.log(`${myVar[2]} is undefined`);
 } else if (myVar.length === 4) {
-    console.log(`${myVar[3]} is ${myVar[4]}`);
+    console.log(`${myVar[2]} is ${myVar[3]}`);
 }
