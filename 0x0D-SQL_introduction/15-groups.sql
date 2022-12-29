ff5545d75b830fda59767f1b191f1cd1f11f5c01
FrancisOkolo <francischinedu78@gmail.com>
2022-12-29 10:56:05 -0800

    task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
 -- Task 15
 -- Query to determine number of ocuurence of records
-SELECT score, COUNT(score) AS 'number'
+SELECT score, COUNT(score) AS number
 FROM second_table
-ORDER BY 'number' DESC;
+GROUP BY score
+ORDER BY number DESC;
