aca348f98fb8453593f061182fb560844124e025
FrancisOkolo <francischinedu78@gmail.com>
2022-12-29 10:09:17 -0800

    task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Task 9
+-- Creating and populating a table
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+	);
+
+INSERT INTO second_table VALUES(1, 'John', 10);
+INSERT INTO second_table VALUES(2, 'Alex', 3);
+INSERT INTO second_table VALUES(3, 'Bob', 14);
+INSERT INTO second_table VALUEs(4, George, 8);
