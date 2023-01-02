737691ee515ea2ba7bf215b78d1ec9a88dbb7e5d
FrancisOkolo <francischinedu78@gmail.com>
2023-01-02 02:11:35 -0800

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- TAsk 0
 -- query to show privilages for users in mysql server
-SHOW GRANTS FOR user_0d_1, user_0d_2 @localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_od_2'@'localhost';
