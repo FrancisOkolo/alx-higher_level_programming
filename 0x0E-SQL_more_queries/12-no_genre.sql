946e1f4561c65c181b7784846f6a9eebaadb2724
FrancisOkolo <francischinedu78@gmail.com>
2023-01-02 13:18:37 -0800

    task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Task 12
+-- Query to import a database and show unlinked records from tables in it.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id = NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
