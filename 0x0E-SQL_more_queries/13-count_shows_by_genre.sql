3edf370431a6df48c85e828173f98c52d427495f
FrancisOkolo <francischinedu78@gmail.com>
2023-01-03 02:00:57 -0800

    task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,10 @@
 -- Task 13
 -- Query to import database and use the count function on a table join
-SELECT tv_genres.name AS 'genre', tv_show_genres.genre_id AS 'number_of_shows'
+
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_genres
 JOIN tv_show_genres
-	ON tv.genres.id = tv_show_genre.genre_id
-WHERE tv_genre.name IS NOT NULL
-GROUP BY COUNT(tv_show.genre.id)
-ORDER BY tv_show.genre.id DESC;
+	ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name IS NOT NULL
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
