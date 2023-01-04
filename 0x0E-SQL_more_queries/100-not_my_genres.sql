418bb33d5b3d2c0e183035a09d06ac65372dfa89
FrancisOkolo <francischinedu78@gmail.com>
2023-01-03 20:12:12 -0800

    task 17

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- Advanced Task 17
+-- Query to list all genres not linked to the show Dexter
+SELECT tv_genres.name AS 'name'
+FROM tv_genres
+JOIN tv_show_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+	ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
