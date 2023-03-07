fadd201cb7028064f697b58547af3a6ea5732591
FrancisOkolo <francischinedu78@gmail.com>
2023-03-07 07:15:44 -0800

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+A python functions that lists all attributes
+... of  an object
+'''
+
+def lookup(obj):
+    ''' list object attributes'''
+    att = []
+    att.append(dir(obj))
+    return att
